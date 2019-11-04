68da729d2c7a77a508f915d72c9f177c52eb72bc
MinyeGit <minye.wu.cs@gmail.com>
2019-11-04 13:21:37 -0600

    Add the rotation idea

--- src/Earth.js
+++ src/Earth.js
@@ -6,15 +6,17 @@ class Earth extends Planet {
 		 *		@post constructor
 		 * 		@return none
 		 */
+
     constructor(timescale) {
         super(0, 0, 0, 50, 0, "Earth", "assets/earth.jpg");
         this.orbitX = 0;
         this.orbitY = 0;
         this.orbitZ = 0;
         this.timescale = timescale;
-       
+        this.angle = 0;
          
     }
+
         /**
          * draws things to the program
 		 * @pre none
@@ -32,6 +34,8 @@ class Earth extends Planet {
         this.x = Math.sin(this.orbitX)*1000;
         this.y = 0;
         this.z = Math.cos(this.orbitZ)*1000;
+
+
             
         /*
         camX = -1*Math.sin(rotateX)*camRad + Math.sin(this.orbitX) *1000;
@@ -46,14 +50,15 @@ class Earth extends Planet {
         camera(camX , camY, camZ, 0, 0, 0, 0, 1, 0);
         
         */
-
         push();
             texture(this.texture);
-  		    translate(this.x, this.y, this.z);
+            translate(this.x, this.y, this.z);
+            rotate(this.angle);
             sphere(this.radius, 30, 30);
+            this.angle += 1;
         pop();
         
-         
+        
         this.trail[Math.floor((frameCount%this.trailLength)/(this.trailLength/this.trailPoints))] = [this.x ,this.y ,this.z];
         
         this.drawTrail();
